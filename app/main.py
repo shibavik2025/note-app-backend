f3c324fc4b2d02eded47311aa27b00ecfbc2e6e1
shibavik2025 <shitalbavikar2022@gmail.com>
2025-03-02 17:09:32 +0000

    Added dependancy gunicorn

--- app/main.py
+++ app/main.py
@@ -4,9 +4,6 @@ from fastapi import FastAPI
 from routes import note as note_router, user as user_router
 from app.database import Base, engine
 from fastapi.middleware.cors import CORSMiddleware
-
-
-
 import models.note, models.user
 
 app = FastAPI()
